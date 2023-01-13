19abecd3d7f1ca3767cb8df57805149c3fc14c77
leo9iota <nine_iota1@proton.me>
2023-01-13 23:24:31 +0100

    Update logo

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,7 @@
 import Link from 'next/link';
 import React from 'react';
 import { AiFillBulb, AiFillFileZip, AiFillGithub, AiFillLinkedin } from 'react-icons/ai';
-import { DiCssdeck } from 'react-icons/di';
+import { DiApple, DiChrome, DiCloud9, DiCssdeck, DiLinux } from 'react-icons/di';
 
 import { Container, Span, Div1, Div2, Div3, NavLink, SocialIcons } from './HeaderStyles';
 
@@ -10,7 +10,7 @@ const Header = () =>  (
     <Div1>
       <Link legacyBehavior href='/'>
         <a style={{ display: 'flex', alignItems: 'center', color: 'white', marginBottom: '20px' }}>
-          <DiCssdeck size='3rem' /> <Span>Portfolio</Span>
+          <DiLinux size='3rem' /> <Span>Portfolio</Span>
         </a>
       </Link>
     </Div1>
