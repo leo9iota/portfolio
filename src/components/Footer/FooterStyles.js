fea1eabdcca5129b69b7b25765da3c9d7efa1011
leo9iota <nine_iota1@proton.me>
2023-01-13 21:26:31 +0100

    Update formatting

--- src/components/Footer/FooterStyles.js
+++ src/components/Footer/FooterStyles.js
@@ -1,17 +1,17 @@
-import styled from "styled-components"
+import styled from 'styled-components';
 
 export const FooterWrapper = styled.section`
 	width: calc(100vw - 96px);
-  max-width: 1040px;
-  padding: 2rem 48px 40px;
-  margin: 1rem auto;
-  box-sizing: content-box;
+	max-width: 1040px;
+	padding: 2rem 48px 40px;
+	margin: 1rem auto;
+	box-sizing: content-box;
 
 
-  @media ${props => props.theme.breakpoints.sm} {
-    padding: 0 16px 48px;
-    width: calc(100vw - 32px);
-  }
+	@media ${props => props.theme.breakpoints.sm} {
+		padding: 0 16px 48px;
+		width: calc(100vw - 32px);
+	}
 `
 
 export const LinkItem = styled.a`
@@ -44,35 +44,35 @@ export const LinkItem = styled.a`
 `
 
 export const SocialIconsContainer = styled.div`
-max-width: 1040px;
-display: flex;
-justify-content: space-between;
+	max-width: 1040px;
+	display: flex;
+	justify-content: space-between;
 
-@media ${props => props.theme.breakpoints.md}{
-  display: flex;
-  justify-content: space-between;
-}
+	@media ${props => props.theme.breakpoints.md} {
+		display: flex;
+		justify-content: space-between;
+	}
 
-@media ${props => props.theme.breakpoints.sm}{
-  display: flex;
-	width: 100%;
-  flex-direction: column;
-}
+	@media ${props => props.theme.breakpoints.sm} {
+		display: flex;
+		width: 100%;
+		flex-direction: column;
+	}
 `
 
 export const CompanyContainer = styled.div`
-  display: flex;
+	display: flex;
 	align-items:baseline;
 	flex-wrap: wrap;
 	margin-right: auto;
 	
 
-	@media ${props => props.theme.breakpoints.md}{
+	@media ${props => props.theme.breakpoints.md} {
 		flex-direction: column;
 		align-items: baseline;
 	}
 
-	@media ${props => props.theme.breakpoints.sm}{
+	@media ${props => props.theme.breakpoints.sm} {
 		display: flex;
 		flex-direction: column;
 		margin: 0 0 32px;
@@ -89,12 +89,12 @@ export const Slogan = styled.p`
 	line-height: 30px;
 	padding: 1rem;
 
-	@media ${props => props.theme.breakpoints.md}{
+	@media ${props => props.theme.breakpoints.md} {
 		font-size: 16px;
 		line-height: 28px;
 	}
 
-	@media ${props => props.theme.breakpoints.sm}{
+	@media ${props => props.theme.breakpoints.sm} {
 		line-height: 22px;
 		font-size: 14px;
 		min-width: 100px;
@@ -103,9 +103,9 @@ export const Slogan = styled.p`
 
 export const SocialContainer = styled.div`
 	display: flex;
-  align-items: center;
+	align-items: center;
 
-	@media ${props => props.theme.breakpoints.md}{
+	@media ${props => props.theme.breakpoints.md} {
 		justify-content: center;
 		padding-right: 16px;
 		flex-wrap: wrap;
@@ -115,10 +115,10 @@ export const SocialContainer = styled.div`
 
 export const LinkList = styled.ul`
 	border-top: 1px solid rgba(255, 255, 255, 0.1);
-  display: grid;
+	display: grid;
 	grid-template-columns: repeat(3, minmax(85px, 220px));
 	gap: 40px;
-  padding: 40px 0 28px;
+	padding: 40px 0 28px;
 
 	@media ${props => props.theme.breakpoints.lg} {
 		padding: 32px 0 16px;
