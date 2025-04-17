1e06c4eee8d0ab3bdc51bacb573437ed3a846371
leo9iota <leo9iota@gmail.com>
2025-04-17 17:36:50 +0200

    Add comments and stuff

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-class MatrixDigitalRain {}
+/**
+ * @class MatrixDigitalRain
+ * Digital rain background from The Matrix 
+ */
+class MatrixDigitalRain {
+    
+}
 
+/**
+ * @class Terminal
+ * Interactive terminal inspired by Alacritty
+ */
 class Terminal {}
